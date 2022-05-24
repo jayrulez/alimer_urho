0b6d74a8fe75da034107fb76696b0508627d8c6a
Amer Koleci <amerkoleci@gmail.com>
2022-05-24 16:59:33 +0200

    Remove Clang-Tools and bindings generation.

--- Source/Tools/BindingGenerator/Tuning.h
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2008-2022 the Urho3D project
-// License: MIT
-
-#pragma once
-
-#include <string>
-
-std::string InsideDefine(const std::string& headerFile);
-bool IsIgnoredHeader(const std::string& headerFile);
